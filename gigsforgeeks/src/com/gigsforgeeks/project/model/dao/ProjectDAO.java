e0abff73d9e32eb56355a3bb462658f1e4dd385a
april <newapril.dev@gmail.com>
2020-09-20 23:01:43 +0900

    add ProjectService, ProjectDAO classes

--- gigsforgeeks/src/com/gigsforgeeks/project/model/dao/ProjectDAO.java
+++ gigsforgeeks/src/com/gigsforgeeks/project/model/dao/ProjectDAO.java
@@ -1,5 +1,71 @@
 package com.gigsforgeeks.project.model.dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static com.gigsforgeeks.common.JDBCTemplate.*;
+import com.gigsforgeeks.project.model.vo.Project;
+
 public class ProjectDAO {
+	
+	private Properties properties = new Properties();
+	
+	/**
+	 * SQL문을 모아둔 XML 파일을 읽어온다.
+	 */
+	public ProjectDAO() {
+		
+		String fileName = ProjectDAO.class.getResource("/sql/project/project-mapper.xml").getPath();
+		try {
+			properties.loadFromXML(new FileInputStream(fileName));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/**
+	 * 사용자로부터 등록할 프로젝트의 정보를 전달받아서 
+	 * 해당 프로젝트 정보를 DB에 등록하고 성공 여부를 반환하는 메서드
+	 * 
+	 * @param project 클라이언트 서버의 사용자로부터 전달받은 프로젝트 객체
+	 * @param connection Service 클래스로부터 받은 DB서버와의 Connection 객체
+	 * @return 클라이언트 서버가 요청한 프로젝트 객체 DB서버 등록 성공여부
+	 */
+	public int insertProject(Project project, Connection connection) {
+		
+		int result = 0;
+		
+		PreparedStatement statement = null;
+		String sql = properties.getProperty("insertProject");
+		
+		try {
+			statement = connection.prepareStatement(sql);
+			statement.setString(1, project.getRequiredSkill());
+			statement.setString(2, project.getProjectName());
+			statement.setString(3, project.getDescription());
+			statement.setDate(4, project.getExpectStart());
+			statement.setDate(5, project.getExpectEnd());
+			statement.setString(6, project.getMeansOfPayment());
+			statement.setInt(7, project.getMinBid());
+			statement.setInt(8, project.getMaxBid());
+			statement.setDate(9, project.getEndBid());
+			result = statement.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		} finally {
+			close(statement);
+			
+		}
+		
+		return result;
+		
+	}
 
 }
--- gigsforgeeks/src/com/gigsforgeeks/project/model/service/ProjectService.java
+++ gigsforgeeks/src/com/gigsforgeeks/project/model/service/ProjectService.java
@@ -1,5 +1,36 @@
 package com.gigsforgeeks.project.model.service;
 
-public class ProjectService {
+import static com.gigsforgeeks.common.JDBCTemplate.*;
 
+import java.sql.Connection;
+
+import com.gigsforgeeks.project.model.dao.ProjectDAO;
+import com.gigsforgeeks.project.model.vo.Project;
+
+public class ProjectService {
+	
+	/**
+	 * 사용자로부터 등록할 프로젝트의 정보를 전달받아서 
+	 * 해당 프로젝트 정보를 DB에 등록하고 성공 여부를 반환하는 메서드
+	 * 
+	 * @param project 클라이언트 서버의 사용자로부터 전달받은 프로젝트 객체
+	 * @return 클라이언트 서버가 요청한 프로젝트 객체 DB서버 등록 성공여부
+	 */
+	public int insertProject(Project project) {
+		
+		Connection connection = getConnection();
+		int result = new ProjectDAO().insertProject(project, connection);
+		
+		if(result > 0) {
+			commit(connection);
+		}else {
+			rollback(connection);
+		}
+		
+		close(connection);
+		
+		return result;
+		
+	}
+	
 }
