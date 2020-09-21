bf535e869c251b786d048506dd7e43ca10176477
LeehaEun95 <67348616+LeehaEun95@users.noreply.github.com>
2020-09-21 17:10:17 +0900

    member.vo -하은
    member.vo

--- gigsforgeeks/src/com/gigsforgeeks/member/model/vo/Member.java
+++ gigsforgeeks/src/com/gigsforgeeks/member/model/vo/Member.java
@@ -1,5 +1,223 @@
 package com.gigsforgeeks.member.model.vo;
 
+import java.sql.Date;
+
 public class Member {
 
+	private int userNo;					// 회원번호
+	private String userId;				// 회원아이디
+	private String userEmail;			// 회원이메일
+	private String userPwd;				// 회원 비밀번호
+	private Date enrollDate;			// 가입일
+	private Date outrpllDate;			// 탈퇴일
+	private String status;				// 탈퇴여부
+	private String membership;			// 멤버십여부
+	private String excellentFreelance;	// 우수 프리랜서 여부
+	private String blackList;			// 신고회원
+	private String identifyEmail;		// 이메일 인증 여부
+	private String admin;				// 관리자 여부
+	private String userType;			// 계정여부
+	private String jobTitle;			// 직급명
+	private String company;				// 회사명
+	private String skill;				// 기술
+	private int payRate;				// 희망시급
+	private String profileImage;		// 프로필 이미지 (파일명)
+	private String location;			// 지역
+	
+	
+	public Member() {
+		
+	}
+	
+	public Member(int userNo, String userId, String userEmail, String userPwd, Date enrollDate, Date outrpllDate,
+			String status, String membership, String excellentFreelance, String blackList, String identifyEmail,
+			String admin, String userType, String jobTitle, String company, String skill, int payRate,
+			String profileImage, String location) {
+		super();
+		this.userNo = userNo;
+		this.userId = userId;
+		this.userEmail = userEmail;
+		this.userPwd = userPwd;
+		this.enrollDate = enrollDate;
+		this.outrpllDate = outrpllDate;
+		this.status = status;
+		this.membership = membership;
+		this.excellentFreelance = excellentFreelance;
+		this.blackList = blackList;
+		this.identifyEmail = identifyEmail;
+		this.admin = admin;
+		this.userType = userType;
+		this.jobTitle = jobTitle;
+		this.company = company;
+		this.skill = skill;
+		this.payRate = payRate;
+		this.profileImage = profileImage;
+		this.location = location;
+	}
+
+
+	public int getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getUserPwd() {
+		return userPwd;
+	}
+
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+
+	public Date getEnrollDate() {
+		return enrollDate;
+	}
+
+	public void setEnrollDate(Date enrollDate) {
+		this.enrollDate = enrollDate;
+	}
+
+	public Date getOutrpllDate() {
+		return outrpllDate;
+	}
+
+	public void setOutrpllDate(Date outrpllDate) {
+		this.outrpllDate = outrpllDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getMembership() {
+		return membership;
+	}
+
+	public void setMembership(String membership) {
+		this.membership = membership;
+	}
+
+	public String getExcellentFreelance() {
+		return excellentFreelance;
+	}
+
+	public void setExcellentFreelance(String excellentFreelance) {
+		this.excellentFreelance = excellentFreelance;
+	}
+
+	public String getBlackList() {
+		return blackList;
+	}
+
+	public void setBlackList(String blackList) {
+		this.blackList = blackList;
+	}
+
+	public String getIdentifyEmail() {
+		return identifyEmail;
+	}
+
+	public void setIdentifyEmail(String identifyEmail) {
+		this.identifyEmail = identifyEmail;
+	}
+
+	public String getAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(String admin) {
+		this.admin = admin;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getSkill() {
+		return skill;
+	}
+
+	public void setSkill(String skill) {
+		this.skill = skill;
+	}
+
+	public int getPayRate() {
+		return payRate;
+	}
+
+	public void setPayRate(int payRate) {
+		this.payRate = payRate;
+	}
+
+	public String getProfileImage() {
+		return profileImage;
+	}
+
+	public void setProfileImage(String profileImage) {
+		this.profileImage = profileImage;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Member [userNo=" + userNo + ", userId=" + userId + ", userEmail=" + userEmail + ", userPwd=" + userPwd
+				+ ", enrollDate=" + enrollDate + ", outrpllDate=" + outrpllDate + ", status=" + status + ", membership="
+				+ membership + ", excellentFreelance=" + excellentFreelance + ", blackList=" + blackList
+				+ ", identifyEmail=" + identifyEmail + ", admin=" + admin + ", userType=" + userType + ", jobTitle="
+				+ jobTitle + ", company=" + company + ", skill=" + skill + ", payRate=" + payRate + ", profileImage="
+				+ profileImage + ", location=" + location + "]";
+	}
+	
+	
 }
