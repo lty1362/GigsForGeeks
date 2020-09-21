52a52af2cea936d642fb5deeaaaf616900c42f8c
LeehaEun95 <67348616+LeehaEun95@users.noreply.github.com>
2020-09-21 22:05:28 +0900

    memberService, mamberDAO -하은
    memberService, mamberDAO (login)

--- gigsforgeeks/src/com/gigsforgeeks/member/model/dao/MemberDAO.java
+++ gigsforgeeks/src/com/gigsforgeeks/member/model/dao/MemberDAO.java
@@ -1,5 +1,78 @@
 package com.gigsforgeeks.member.model.dao;
 
-public class MemberDAO {
+import static com.gigsforgeeks.common.JDBCTemplate.close;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
 
+import com.gigsforgeeks.member.model.vo.Member;
+
+public class MemberDAO {
+	
+	private Properties prop = new Properties();
+	
+	public MemberDAO() {
+		
+		String filName = MemberDAO.class.getResource("/sql/member/member-mapper.xml").getPath();
+		
+		try {
+			prop.loadFromXML(new FileInputStream(filName));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	/*login*/
+	public Member loginMember(Connection conn, String userId, String userPwd) {
+		
+		Member m = null;
+		
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		
+		String sql = prop.getProperty("loginMember");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, userId);
+			pstmt.setString(2, userPwd);
+			
+			rset = pstmt.executeQuery();
+			
+			if(rset.next()) {
+				m = new Member(rset.getString("USER_ID"),
+						       rset.getString("USER_EMAIL"),
+						       rset.getString("USER_PWD"),
+						       rset.getDate("ENROLL_DATE"),
+						       rset.getDate("OUTROLL_DATE"),
+						       rset.getString("STATUS"),
+						       rset.getString("MEMBERSHIP"),
+						       rset.getString("EXCELLENT_FREELANCE"),
+						       rset.getString("BLACKLIST"),
+						       rset.getString("IDENTIFY_EMAIL"),
+						       rset.getString("ADMIN"),
+						       rset.getString("USER_TYPE"),
+						       rset.getString("JOB_TITLE"),
+						       rset.getString("COMPANY"),
+						       rset.getString("SKILL"),
+						       rset.getInt("PAY_RATE"),
+						       rset.getString("PROFILE_IMAGE"),
+						       rset.getString("LOCATION")
+							   );
+				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(rset);
+			close(pstmt);
+		}
+		
+		return m;
+	}
+	
 }
--- gigsforgeeks/src/com/gigsforgeeks/member/model/service/memberService.java
+++ gigsforgeeks/src/com/gigsforgeeks/member/model/service/memberService.java
@@ -1,5 +1,25 @@
 package com.gigsforgeeks.member.model.service;
 
+import static com.gigsforgeeks.common.JDBCTemplate.close;
+import static com.gigsforgeeks.common.JDBCTemplate.getConnection;
+
+import java.sql.Connection;
+
+import com.gigsforgeeks.member.model.dao.MemberDAO;
+import com.gigsforgeeks.member.model.vo.Member;
+
+
 public class memberService {
 
+	/*login*/
+	public Member loginMember(String userId, String userPwd) {
+		
+		Connection conn = getConnection();
+		
+		Member m = new MemberDAO().loginMember(conn, userId, userPwd);
+		
+		close(conn);
+		
+		return m;
+	}
 }
