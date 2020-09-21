88ed1d066807ea56360b5bb8b42a69030c549cfd
LeehaEun95 <67348616+LeehaEun95@users.noreply.github.com>
2020-09-21 22:14:00 +0900

    loginServelt, member-mapper.xml  -하은
    loginServelt, member-mapper.xml

---
+++ gigsforgeeks/src/com/gigsforgeeks/member/controller/loginServlet.java
@@ -0,0 +1,67 @@
+package com.gigsforgeeks.member.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.gigsforgeeks.member.model.service.memberService;
+import com.gigsforgeeks.member.model.vo.Member;
+
+/**
+ * Servlet implementation class loginServlet
+ */
+@WebServlet("/login.me")
+public class loginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public loginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		String userId = request.getParameter("userId");
+		String userPwd = request.getParameter("userPwd");
+		
+		Member m = new memberService().loginMember(userId, userPwd);
+		
+		if(m != null) {
+			
+			 HttpSession session = request.getSession();
+	    	 session.setAttribute("m", m);
+			
+	    	 response.sendRedirect(request.getContextPath());
+			
+		}else {
+			
+			request.getSession().setAttribute("errorMsg", "로그인에 실패했습니다.");
+			RequestDispatcher view = request.getRequestDispatcher("views/common/errorPage.jsp");
+			view.forward(request, response);
+			
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
