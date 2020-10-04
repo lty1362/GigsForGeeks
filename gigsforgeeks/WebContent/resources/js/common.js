a7ee4153dbc4ca8c8062631b5d73f88d50212e36
april <newapril.dev@gmail.com>
2020-10-04 21:47:44 +0900

    common.js 에 프로젝트 상세보기 시 쿼리 스트링 추가

--- gigsforgeeks/WebContent/resources/js/common.js
+++ gigsforgeeks/WebContent/resources/js/common.js
@@ -1,2 +1,12 @@
-
-
+/* Project Table */
+$(document).ready(function(){
+	
+	// 프로젝트 상세보기 시 쿼리 스트링 (ProjectId 값)
+	$(".projectTable>tbody>tr>td>a").click(function(){
+	
+		var projectId = $(this).next().text();
+		$(this).attr("href", contextPath + "/detail.proj?projectId=" + projectId);
+		
+	});
+	
+});
